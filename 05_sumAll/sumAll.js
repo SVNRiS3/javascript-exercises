959b88cf0f8a2913056dc514d048a066b70093fa
Chris <krzy.driwa@gmail.com>
2023-09-06 22:57:35 +0200

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, lastNumber) {
+    let sum = 0;
+    if (firstNumber < 0 || typeof firstNumber != 'number' || lastNumber < 0 || typeof lastNumber != 'number')
+        return "ERROR";
+    let sortedNumbers = [firstNumber, lastNumber].sort(function (a, b) { return a - b });
+    while (sortedNumbers[0] <= sortedNumbers[1]) {
+        sum += sortedNumbers[0];
+        sortedNumbers[0]++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
