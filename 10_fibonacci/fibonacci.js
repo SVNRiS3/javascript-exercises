d7147e42bc2c610481c4911f73ce7d4a9f180e2c
SVNRiS3 <krzy.driwa@gmail.com>
2023-10-10 15:22:48 +0200

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number < 0) return "OOPS";
+    let temp1 = 1;
+    let sum = 0;
+    let temp2 = 1;
+    let n = 1;
+    do {
+        if (n > 1)
+            temp1 = sum;
+        sum += temp2;
+        temp2 = temp1;
+        n++;
+    }
+    while (n < number)
+    return sum;
+    //1 1 2 3 5 8
 };
 
 // Do not edit below this line
