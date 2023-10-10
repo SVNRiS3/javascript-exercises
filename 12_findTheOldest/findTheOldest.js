cd5e9f617036e7d10ffc95d0c04eb295bea655ee
SVNRiS3 <krzy.driwa@gmail.com>
2023-10-10 16:18:54 +0200

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+	return people.reduce(
+		(oldest, person) => {
+			let endYear = person.yearOfDeath
+				? person.yearOfDeath
+				: new Date().getFullYear();
+			let yearsLived = endYear - person.yearOfBirth;
+			if (oldest.age < yearsLived) {
+				oldest.name = person.name;
+				oldest.age = yearsLived;
+			}
+			return oldest;
+		},
+		{ name: "", age: 0 }
+	);
 };
 
 // Do not edit below this line
